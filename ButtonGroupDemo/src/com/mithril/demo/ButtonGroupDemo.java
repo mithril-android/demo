0c9a19103b07a1a9b0da782ce591687a966262a3
Kishore <abyu.kishore@gmail.com>
2015-01-08 09:32:25 +0530

    New demo app for ButtonGroup

---
+++ ButtonGroupDemo/src/com/mithril/demo/ButtonGroupDemo.java
@@ -0,0 +1,17 @@
+package com.mithril.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ButtonGroupDemo extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+
+}
